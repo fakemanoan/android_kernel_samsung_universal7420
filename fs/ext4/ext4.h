cc2f82fa1e135b3e365158dcc7880b5605340182
Theodore Ts'o <tytso@mit.edu>
2013-10-17 21:11:01 -0400

    ext4: add ratelimiting to ext4 messages
    In the case of a storage device that suddenly disappears, or in the
    case of significant file system corruption, this can result in a huge
    flood of messages being sent to the console.  This can overflow the
    file system containing /var/log/messages, or if a serial console is
    configured, this can slow down the system so much that a hardware
    watchdog can end up triggering forcing a system reboot.
    
    Google-Bug-Id: 7258357
    
    Signed-off-by: "Theodore Ts'o" <tytso@mit.edu>

--- fs/ext4/ext4.h
+++ fs/ext4/ext4.h
@@ -30,6 +30,7 @@
 #include <linux/wait.h>
 #include <linux/blockgroup_lock.h>
 #include <linux/percpu_counter.h>
+#include <linux/ratelimit.h>
 #include <crypto/hash.h>
 #ifdef __KERNEL__
 #include <linux/compat.h>
@@ -1392,6 +1393,11 @@ struct ext4_sb_info {
 	/* Debugging info. for backing device reset */
 	__u32 s_bd_reset_cnt;
 	__u8 s_bd_reset_time[16];
+
+	/* Ratelimit ext4 messages. */
+	struct ratelimit_state s_err_ratelimit_state;
+	struct ratelimit_state s_warning_ratelimit_state;
+	struct ratelimit_state s_msg_ratelimit_state;
 };
 
 static inline struct ext4_sb_info *EXT4_SB(struct super_block *sb)
