4a978d319e8b2a230d878c71a542118dc57397f7
Theodore Ts'o <tytso@mit.edu>
2014-09-03 09:33:00 -0400

    ext4: avoid trying to kfree an ERR_PTR pointer
    Thanks to Dan Carpenter for extending smatch to find bugs like this.
    (This was found using a development version of smatch.)
    
    Fixes: 36de928641ee48b2078d3fe9514242aaa2f92013
    Reported-by: Dan Carpenter <dan.carpenter@oracle.com
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    Cc: stable@vger.kernel.org
    [ TALU - Change to ext4_cross_rename ignored - Doesn't exist ]
    Signed-off-by: TALU <taluemail272@gmail.com>

--- fs/ext4/namei.c
+++ fs/ext4/namei.c
@@ -3223,6 +3223,7 @@ static int ext4_rename(struct inode *old_dir, struct dentry *old_dentry,
 				 &new.de, &new.inlined);
 	if (IS_ERR(new.bh)) {
 		retval = PTR_ERR(new.bh);
+		new.bh = NULL;
 		goto end_rename;
 	}
 	if (new.bh) {
